dcb433ed6440ebfba859ccfb64ff1258382c3872
dishapasari <84625603+dishapasari@users.noreply.github.com>
2021-05-24 13:47:19 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+  
+<HTML>
+<head>
+	<center><title>HSBC by DISHA</title></center>
+	<link rel = "stylesheet" type = "text/css" href = "HeaderButton.css">
+</head>
+<body>
+
+<body background= "back.jpg">
+<body >
+	<div id="header">
+       <center><h2 style=" font-family:Agency FB; font-size: 55px;color:#380036; float: center;">WELCOME TO HSBC BANK</h2><center/>
+	   <center><h3 style=" font-family:Agency FB; font-size: 25px;color:#380033; float: center;">TSF Project by DISHA PASARI</h3><center/>
+	   <img src="bank.png"width="230" height="230">
+    </div>
+
+
+
+<link rel = "stylesheet" type = "text/css" href = "style.css">
+
+
+<hr style="border: 1px solid lightseagreen">
+	<div align="center" style="top:0px">
+		<table width="1330">
+            		
+			<table width="1316" align="center" class = "t">
+                <tr>
+				<td style = "text-align:center"> <button class = "btn1"> Home </button></td> 	
+				<td style = "text-align:center"><a href="Viewcust.php" target="frame"><button class = "btn2">Customers List</button></a></td>
+				<td style = "text-align:center"><a href="Transfermoney.php" target="frame"><button class = "btn2">Transfer Money</button></a></td>
+				<td style = "text-align:center"><a href="Transachistory.php" target="frame"><button class = "btn2">Transaction History</button></a></td>
+                </tr>
+            </table>
+        </table>
+	</div>
+<hr style="border: 1px solid lightseagreen">
+<br><br>
+	
+<br><br>
+<div class="footer">
+<center> <span style="font-family:verdana;">&copy;</span>  2021 HSBC Bank | DISHA PASARI . All rights reserved. </center>
+</div>
+</body>
+</html>
